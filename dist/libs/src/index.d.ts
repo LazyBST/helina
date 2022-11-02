d18910e0f180a2359df11d09b4fd8300aa2200fd
Ayush Arora <ayusharora1778@gmail.com>
2022-11-02 18:23:27 +0530

    chore: updated build

--- dist/libs/src/index.d.ts
+++ dist/libs/src/index.d.ts
@@ -5,3 +5,4 @@ export * from './constants';
 export * from './interfaces';
 export * from './rest';
 export * from './exceptions';
+export * from './logger';
--- dist/libs/src/logger/index.js
+++ dist/libs/src/logger/index.js
@@ -14,12 +14,6 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./module"), exports);
-__exportStar(require("./service"), exports);
-__exportStar(require("./utils"), exports);
-__exportStar(require("./constants"), exports);
-__exportStar(require("./interfaces"), exports);
-__exportStar(require("./rest"), exports);
-__exportStar(require("./exceptions"), exports);
-__exportStar(require("./logger"), exports);
+__exportStar(require("./logger.module"), exports);
+__exportStar(require("./logger.service"), exports);
 //# sourceMappingURL=index.js.map
