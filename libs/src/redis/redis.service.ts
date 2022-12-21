352b5a91191aa8b7b6f11b37739105c94026270b
Rohan Raman <rohan.raman@original4sure.com>
2022-12-21 17:26:33 +0530

    parsed redis port as number

--- libs/src/redis/redis.service.ts
+++ libs/src/redis/redis.service.ts
@@ -7,7 +7,7 @@ export class RedisService {
   private readonly _redis: Redis;
   constructor(private readonly config: ConfigService) {
     this._redis = new Redis({
-      port: +config.get('REDIS_PORT'),
+      port: parseInt(config.get('REDIS_PORT')),
       host: config.get('REDIS_HOST'),
       password: config.get('REDIS_PASS'),
     });
